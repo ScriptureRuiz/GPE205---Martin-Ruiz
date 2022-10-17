c45daee60a7728f7f3f66bc68868903d892f48bf
Martin Ruiz <105525646+ScriptureRuiz@users.noreply.github.com>
2022-10-16 23:11:21 -0500

    Player Spawn
    The player spawn was created and connected to the GameManager, next I need to code the components to be attached.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,25 +8,43 @@ public class GameManager : MonoBehaviour
      Static means that we can access our single instance of the class from anywhere.
      This variable will point back at this component when storing data in this variable */
     public static GameManager instance;
-
-   // This will run before any start() function in our game
-   // Whatever code we write here will run before any object can run their start() function
-   private void Awake()
+    /* A variable that shows in the inspector that allows us to connect our spawner gameObject to spawn the player.
+        We will also use the transform of this object for position*/
+    public Transform playerSpawnTransform;
+    // This will run before any start() function in our game
+    // Whatever code we write here will run before any object can run their start() function
+    private void Awake()
     {
         /* This conditional statement checks to see if another GameManeger
            object exist and destroys it if so*/
-        if (instance==null)
+        if (instance == null)
         {
-          //This variable will point back at this component when storing data in this variable
-           instance = this;
+            //This variable will point back at this component when storing data in this variable
+            instance = this;
             // This will keep our GameManager from getting destroyed when loading a new scene
             DontDestroyOnLoad(gameObject);
+            // Here we spawn in the player controller at (0,0,0) and no rotation
+            GameObject newplayerObj = Instantiate(PlayerControllerPrefab, Vector3.zero, Quaternion.identity) as GameObject;
+            /* Instantiats is when you create a new object in C# for a class using the new keyword
+              It spawns a tankPawn from the prefab at the player spawn transform position and gives it the player spawn transform rotation and makes it a game object*/
+            GameObject newPawnObj = Instantiate(TankPawnPrefab,playerSpawnTransform.position, playerSpawnTransform.rotation) as GameObject;
+
         }
         else
         {
-            // There is already an instance, so destroy this gameObject
+            // There is already an instance, so it destroys this gameObject
             Destroy(gameObject);
-        }
+        } }
 
-    }
+ 
+   
+
+
+ //Prefabs
+    public GameObject PlayerControllerPrefab;
+    public GameObject TankPawnPrefab;
+
+       
+
+    
 }
